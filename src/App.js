c6685a536b3fcf45f93c636d00e37c82a896521c
shaunorpen <shaunorpen@users.noreply.github.com>
2019-12-17 13:42:59 +0000

    WIP

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import React from 'react'
-// import logo from './logo.svg'
 import './App.css'
-// import { Button } from 'antd-mobile'
-import Signup from './components/Signup'
+import Register from './components/Register/Register'
 
 function App () {
   return (
     <div className='App'>
-      {/* <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          @mogwai, @shaunorpen and @kvothe waz here.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-      <Button>Start!</Button> */}
-      <Signup />
+      <Register />
     </div>
   )
 }
