3e94dc250968c8b7e9f12457fed97a4b4c7ac759
shaunorpen <shaunorpen@users.noreply.github.com>
2019-12-18 15:34:03 +0000

    validate password mismatch

--- src/__tests__/Register.js
+++ src/__tests__/Register.js
@@ -3,7 +3,9 @@
 import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import user from '@testing-library/user-event'
 import { getQueriesForElement } from '@testing-library/dom'
+import { render } from '@testing-library/react'
 import Register from '../components/Register/Register'
 
 describe('register form', () => {
@@ -33,5 +35,18 @@ describe('register form', () => {
         const button = div.querySelector('a[role="button"]')
         expect(button).toContainHTML('<span>Register</span>')
         expect(button).toBeVisible()
+    })
+})
+
+describe('render form functionality', () => {
+    test('validates password mismatch', () => {
+        const { getByPlaceholderText, getByText } = render(<Register />)
+        const phoneNumber = getByPlaceholderText(/phone number/i)
+        const password = getByPlaceholderText(/^password$/i)
+        const confirmPassword = getByPlaceholderText(/confirm password/i)
+        user.type(phoneNumber, '1')
+        user.type(password, '123456')
+        user.type(confirmPassword, '23456')
+        expect(getByText(/passwords do not match!/i)).toBeVisible()
     })
 })
