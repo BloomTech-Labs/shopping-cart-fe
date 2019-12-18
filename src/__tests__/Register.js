d52755c105d47f46f8cc15c03885765169127e42
shaunorpen <shaunorpen@users.noreply.github.com>
2019-12-18 14:46:52 +0000

    Tidy up existing tests

--- src/__tests__/Register.js
+++ src/__tests__/Register.js
@@ -3,41 +3,35 @@
 import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { getQueriesForElement } from '@testing-library/dom'
 import Register from '../components/Register/Register'
 
 describe('register form', () => {
-  test('has an input for phone number', () => {
     const div = document.createElement('div')
     ReactDOM.render(<Register />, div)
-    const input = div.querySelector('input')
-    expect(input).toHaveAttribute('type', 'text')
-    expect(input).toHaveAttribute('placeholder', 'Phone number')
-    expect(input).toBeEnabled()
-  })
+    const { getByPlaceholderText } = getQueriesForElement(div)
 
-  test('has an input for password', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<Register />, div)
-    const input = div.querySelector('div:nth-child(3) input')
-    expect(input).toHaveAttribute('type', 'password')
-    expect(input).toHaveAttribute('placeholder', 'Password')
-    expect(input).toBeEnabled()
-  })
+    test('has an input for phone number', () => {
+        const input = getByPlaceholderText(/phone number/i)
+        expect(input).toHaveAttribute('type', 'text')
+        expect(input).toBeEnabled()
+    })
 
-  test('has an input for password confirmation', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<Register />, div)
-    const input = div.querySelector('div:nth-child(5) input')
-    expect(input).toHaveAttribute('type', 'password')
-    expect(input).toHaveAttribute('placeholder', 'Confirm password')
-    expect(input).toBeEnabled()
-  })
+    test('has an input for password', () => {
+        const input = getByPlaceholderText(/^password$/i)
+        expect(input).toHaveAttribute('type', 'password')
+        expect(input).toBeEnabled()
+    })
 
-  test('has a register button', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<Register />, div)
-    const button = div.querySelector('a[role="button"]')
-    expect(button).toContainHTML('<span>Register</span>')
-    expect(button).toBeVisible()
-  })
+    test('has an input for password confirmation', () => {
+        const input = getByPlaceholderText(/confirm password/i)
+        expect(input).toHaveAttribute('type', 'password')
+        expect(input).toBeEnabled()
+    })
+
+    test('has a register button', () => {
+        const button = div.querySelector('a[role="button"]')
+        expect(button).toContainHTML('<span>Register</span>')
+        expect(button).toBeVisible()
+    })
 })
