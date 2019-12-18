0cfb466a10ffee8fef29b9aa0797bf1ed39860f0
mogwai <feelirabs@gmail.com>
2019-12-18 15:57:34 +0100

    Add Input Items Used Ant Design and inline styling to complete elements

--- src/login.js
+++ src/login.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './login.css';
 import logo from './shoplogo.gif';
+import List from './input';
 
 function Login () {
   return (
     <div class="login">
         <img src={logo} className="logo" alt="Logo" />
         <p className="headers">Log in</p>
+        <List/>
     </div>
   )
 }
