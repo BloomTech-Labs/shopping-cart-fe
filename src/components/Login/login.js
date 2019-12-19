5d2cc1ae6c3fb25ac408fae27d8fb605ea668a42
shaunorpen <shaunorpen@users.noreply.github.com>
2019-12-19 09:58:56 +0000

    Fix formatting using standardjs

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './login.css';
+import React from 'react'
+import './login.css'
 import logo from '../../images/PureRetail_Logo.png'
 import List from './input'
 
 function Login () {
   return (
-    <div class="login">
-        <img src={logo} className="logo" alt="Logo" />
-        <p className="headers">Log in</p>
-        <List/>
+    <div class='login'>
+      <img src={logo} className='logo' alt='Logo' />
+      <p className='headers'>Log in</p>
+      <List />
     </div>
   )
 }
