a5b0901d3e15fef72cce83f12183d6dca4f55b31
Shaun Orpen <shaunorpen@Shauns-MacBook-Pro.local>
2020-01-06 15:24:42 +0000

    Add register tests

--- src/__tests__/Register.js
+++ src/__tests__/Register.js
@@ -1,64 +1,46 @@
-/* eslint-env jest */
-
 import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
-import ReactDOM from 'react-dom'
-import user from '@testing-library/user-event'
-import { getQueriesForElement } from '@testing-library/dom'
 import { render } from '@testing-library/react'
-import Register from '../components/Register/Register'
+import Register from '../components/register'
 
-describe('register form', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<Register />, div)
-  const { getByPlaceholderText } = getQueriesForElement(div)
-
-  test('has an input for phone number', () => {
-    const input = getByPlaceholderText(/phone number/i)
-    expect(input).toHaveAttribute('type', 'text')
-    expect(input).toBeEnabled()
-  })
-
-  test('has an input for password', () => {
-    const input = getByPlaceholderText(/^password$/i)
-    expect(input).toHaveAttribute('type', 'password')
-    expect(input).toBeEnabled()
-  })
-
-  test('has an input for password confirmation', () => {
-    const input = getByPlaceholderText(/confirm password/i)
-    expect(input).toHaveAttribute('type', 'password')
-    expect(input).toBeEnabled()
-  })
-
-  test('has a register button', () => {
-    const button = div.querySelector('a[role="button"]')
-    expect(button).toContainHTML('<span>Register</span>')
-    expect(button).toBeVisible()
-  })
-})
-
-describe('render form functionality', () => {
-  test('validates password mismatch', () => {
-    const { getByPlaceholderText, getByText } = render(<Register />)
-    const phoneNumber = getByPlaceholderText(/phone number/i)
-    const password = getByPlaceholderText(/^password$/i)
-    const confirmPassword = getByPlaceholderText(/confirm password/i)
-    user.type(phoneNumber, '1')
-    user.type(password, '123456')
-    user.type(confirmPassword, '23456')
-    expect(getByText(/passwords do not match!/i)).toBeVisible()
-  })
-  test('allows valid registration', () => {
-    const { getByPlaceholderText, getByText } = render(<Register />)
-    const phoneNumber = getByPlaceholderText(/phone number/i)
-    const password = getByPlaceholderText(/^password$/i)
-    const confirmPassword = getByPlaceholderText(/confirm password/i)
-    const registerButton = getByText(/^Register$/)
-    user.type(phoneNumber, '123456789')
-    user.type(password, '123456')
-    user.type(confirmPassword, '123456')
-    user.click(registerButton)
-    expect(alert)
-  })
+describe('login screen', () => {
+    test('renders the logo', () => {
+        const { getByAltText } = render(<Register />)
+        const logo = getByAltText(/pureretail logo/i)
+        expect(logo).toBeVisible()
+    })
+    test('renders the title', () => {
+        const { getByText } = render(<Register />)
+        const title = getByText(/register new/i)
+        expect(title).toBeVisible()
+    })
+    test('renders the phone number input', () => {
+        const { getByPlaceholderText } = render(<Register />)
+        const phoneNumberField = getByPlaceholderText(/phone number/i)
+        expect(phoneNumberField).toBeVisible()
+        expect(phoneNumberField).toBeEnabled()
+    })
+    test('renders the password input', () => {
+        const { getByPlaceholderText } = render(<Register />)
+        const passwordField = getByPlaceholderText(/^password$/i)
+        expect(passwordField).toBeVisible()
+        expect(passwordField).toBeEnabled()
+    })
+    test('renders the confirm password input', () => {
+        const { getByPlaceholderText } = render(<Register />)
+        const confirmPasswordField = getByPlaceholderText(/confirm password/i)
+        expect(confirmPasswordField).toBeVisible()
+        expect(confirmPasswordField).toBeEnabled()
+    })
+    test('renders the register button', () => {
+        const { getByRole } = render(<Register />)
+        const registerButton = getByRole("button")
+        expect(registerButton).toBeVisible()
+        expect(registerButton).toBeEnabled()
+    })
+    test('renders the login link', () => {
+        const { getByText } = render(<Register />)
+        const loginLink = getByText("login")
+        expect(loginLink).toBeVisible()
+    })
 })
