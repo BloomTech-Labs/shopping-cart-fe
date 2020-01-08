c00b7d0c002f0c1b9d2a159c0db598e9fc6edeac
mogwai <feelirabs@gmail.com>
2020-01-08 01:49:26 +0100

    Completed Mobile Menu Footer Note: not wired to any particular URLs yet

--- src/components/DashboardHome/Dashboard.js
+++ src/components/DashboardHome/Dashboard.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import './Dashboard.css';
+import Footer from './Footer'
 
-const Dashboard = ()=> {
-    return 'Hello World!'
+const Dashboard = ()=>{
+    return (
+        <div className='mainDiv'>
+            <div className='welcomeHeader'>Welcome, <br/><span className='name'>Tolu!</span>
+            <p id="storeUrl">vintagestamps.pureretail.com</p>
+            </div>
+            <div className='dashDiv'></div>
+            <Footer/>
+            
+
+        </div>
+    )
 }
 
 export default Dashboard;
