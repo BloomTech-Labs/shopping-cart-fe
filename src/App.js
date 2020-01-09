f926b6fca2946b614579ec214292029a9de32cb0
mogwai <feelirabs@gmail.com>
2020-01-09 13:56:43 +0100

    Put Dashboard component in protected route

--- src/App.js
+++ src/App.js
@@ -9,12 +9,8 @@ import CreateStoreForm from './components/createStore/firstView'
 import AddLogoForm from './components/createStore/addLogo'
 import EditProfile from './components/EditProfile/EditProfile'
 import CreateItem from './components/CreateItem'
-<<<<<<< HEAD
-import Inventory from './components/inventory/inventory'
-import Dashboard from './components/DashboardHome/Dashboard'
-=======
 import PrivateRoute from './components/Auth/PrivateRoute'
->>>>>>> 54e8346e48147a2f0108c917f183f1e5d97472d4
+import Dashboard from './components/DashboardHome/Dashboard'
 // import Inventory from './components/inventory/inventory'
 import Main from './components/inventory'
 
@@ -28,36 +24,13 @@ function App() {
       <Route exact path='/'>
         <LoginForm />
       </Route>
-<<<<<<< HEAD
-      <Route path='/resetpassword'>
-        <ResetPasswordForm />
-      </Route>
-      <Route path='/setnewpassword'>
-        <SetNewPasswordForm />
-      </Route>
-      <Route path='/createstore'>
-        <CreateStoreForm />
-      </Route>
-      <Route path='/addlogo'>
-        <AddLogoForm />
-      </Route>
-      <Route path='/profile'>
-        <EditProfile />
-      </Route>
-      <Route path='/createitem'>
-        <CreateItem />
-      </Route>
-      <Route path='/dashboard'>
-        <Dashboard />
-      </Route>
-=======
       <PrivateRoute path='/resetpassword' component={ResetPasswordForm} />
       <PrivateRoute path='/setnewpassword' component={SetNewPasswordForm} />
       <PrivateRoute path='/createstore' component={CreateStoreForm} />
       <PrivateRoute path='/addlogo' component={AddLogoForm} />
       <PrivateRoute path='/profile' component={EditProfile} />
       <PrivateRoute path='/createitem' component={CreateItem} />
->>>>>>> 54e8346e48147a2f0108c917f183f1e5d97472d4
+      <PrivateRoute path='/dashboard' component={Dashboard} />
     </Switch>
   )
 }
