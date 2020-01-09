6661093a5aa56378d1b292347696b04e24f11fa1
mogwai <feelirabs@gmail.com>
2020-01-09 13:40:36 +0100

    Fix basic formatting errors with Standardjs

--- src/components/DashboardHome/DashContent.js
+++ src/components/DashboardHome/DashContent.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import { Tabs } from 'antd';
-import './Dashboard.css';
-import Pane1 from './Pane1';
+import React from 'react'
+import { Tabs } from 'antd'
 import './Dashboard.css'
+import Pane1 from './Pane1'
 
-const { TabPane } = Tabs;
+const { TabPane } = Tabs
 
-const Content = ()=> {
-    return (
-        <div>
-  <Tabs defaultActiveKey="1" className="content">
-    <TabPane tab="Overview" key="1" >
-      <Pane1/>
-    </TabPane>
-    <TabPane tab="Sales History" key="2">
+const Content = () => {
+  return (
+    <div>
+      <Tabs defaultActiveKey='1' className='content'>
+        <TabPane tab='Overview' key='1'>
+          <Pane1 />
+        </TabPane>
+        <TabPane tab='Sales History' key='2'>
       No earnings yet!
-    </TabPane>
-  </Tabs>
-  </div>
-)
-};
+        </TabPane>
+      </Tabs>
+    </div>
+  )
+}
 
 export default Content
--- src/components/DashboardHome/Pane1.js
+++ src/components/DashboardHome/Pane1.js
@@ -1,22 +1,19 @@
-import React from 'react';
+import React from 'react'
 
-const Pane1 = ()=> {
-    return (
-        <div>
-            <h2 style={{textAlign: 'left', marginLeft:'40px', paddingTop:'20px'}}>Earnings</h2>
-        <div id='cards'>
-        <div className="Cards">
-            <p style={{color:'white', paddingTop:'40px'}}>This month you earned:</p>
+const Pane1 = () => {
+  return (
+    <div>
+      <h2 style={{ textAlign: 'left', marginLeft: '40px', paddingTop: '20px' }}>Earnings</h2>
+      <div id='cards'>
+        <div className='Cards'>
+          <p style={{ color: 'white', paddingTop: '40px' }}>This month you earned:</p>
         </div>
-        <div className="Cards2">
-            <p style={{color:'white', paddingTop:'40px'}}>Lifetime earnings:</p>
+        <div className='Cards2'>
+          <p style={{ color: 'white', paddingTop: '40px' }}>Lifetime earnings:</p>
         </div>
-        <div className="Cards2">
-            <p style={{color:'white', paddingTop:'40px'}}>Lifetime earnings:</p>
-        </div>
-        </div>
-        </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Pane1
