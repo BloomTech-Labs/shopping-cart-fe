f00fde724f1ded13686332535678c2f2792a7903
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-01-16 15:50:27 +0000

    Format with standard

--- src/components/elements/cartHeader.js
+++ src/components/elements/cartHeader.js
@@ -4,17 +4,17 @@ import { Row, Col, Icon, Badge } from 'antd'
 import NoLogo from '../../images/PureRetail_Logo.png'
 
 const CartHeader = () => {
-    return (<Row className='cart-header' type='flex' justify='space-between' align='middle'>
-        <Col span={6} className='logo'>
-            <img src={NoLogo} alt="Store Logo" />
-        </Col>
-        <Col span={12} className='total'>Total: {`$121.44`}</Col>
-        <Col span={6} className='icon'>
-            <Badge count={0} overflowCount={9} showZero>
-                <Icon type="shopping-cart" />
-            </Badge>
-        </Col>
-    </Row>)
+  return (<Row className='cart-header' type='flex' justify='space-between' align='middle'>
+    <Col span={6} className='logo'>
+      <img src={NoLogo} alt='Store Logo' />
+    </Col>
+    <Col span={12} className='total'>Total: {'$121.44'}</Col>
+    <Col span={6} className='icon'>
+      <Badge count={0} overflowCount={9} showZero>
+        <Icon type='shopping-cart' />
+      </Badge>
+    </Col>
+          </Row>)
 }
 
 export default CartHeader
