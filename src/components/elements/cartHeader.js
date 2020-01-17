32d2b892be0e3e85215a32633688aa9433ecae36
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-01-17 15:40:14 +0000

    Add props and back button

--- src/components/elements/cartHeader.js
+++ src/components/elements/cartHeader.js
@@ -3,14 +3,14 @@ import { Row, Col, Icon, Badge } from 'antd'
 
 import NoLogo from '../../images/PureRetail_Logo.png'
 
-const CartHeader = () => {
+const CartHeader = ({ badgeCount = 0, currency = '', totalDue = 0, displayBack = true, displayTotal }) => {
   return (<Row className='cart-header' type='flex' justify='space-between' align='middle'>
     <Col span={6} className='logo'>
-      <img src={NoLogo} alt='Store Logo' />
+      {displayBack ? <Icon type='left-circle' /> : <img src={NoLogo} alt='Store Logo' />}
     </Col>
-    <Col span={12} className='total'>Total: {'$121.44'}</Col>
+    <Col span={12} className='total'>{displayTotal ? `Total: ${currency}${totalDue}` : ''}</Col>
     <Col span={6} className='icon'>
-      <Badge count={0} overflowCount={9} showZero>
+      <Badge count={badgeCount} overflowCount={9} showZero>
         <Icon type='shopping-cart' />
       </Badge>
     </Col>
