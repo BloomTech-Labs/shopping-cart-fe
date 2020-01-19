79116ceacda32e59047434507260a168d30abffd
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-01-19 14:37:48 +0100

    added currency symbol to total

--- src/components/elements/cartHeader.js
+++ src/components/elements/cartHeader.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, Icon, Badge, Input } from 'antd'
@@ -16,8 +16,23 @@ const CartHeader = ({
   displayTotal,
   top = false
 }) => {
+  const [sign, setSign] = useState('')
   const dispatch = useDispatch()
   const cartContents = useSelector(state => state.cart)
+  const storeDetails = useSelector(state => state.user.user)
+  const fixCurrency = (storeDetails) => {
+    if (storeDetails.currency === 'POU') {
+      setSign('£')
+    } else if (storeDetails.currency === 'DOL') {
+      setSign('$')
+    } else if (storeDetails.currency === 'EUR') {
+      setSign('€')
+    } else if (storeDetails.currency === 'YEN') {
+      setSign('¥')
+    } else {
+      return undefined
+    }
+  }
   const totalPrice = (arr) => {
     return arr.reduce((sum, item) => {
       return sum + (item.price * item.quantity)
@@ -31,6 +46,9 @@ const CartHeader = ({
   const change = e => {
     dispatch(creators.setString(e.target.value))
   }
+  useEffect(() => {
+    fixCurrency(storeDetails)
+  }, [storeDetails])
   return (
     <Row className={top ? 'color ' + 'cart-header' : 'cart-header'} type='flex' justify='space-between' align='middle'>
       <Col span={6} className='logo'>
@@ -38,7 +56,7 @@ const CartHeader = ({
           ? <Icon onClick={history.goBack} type='left-circle' />
           : <img src={logoPath || NoLogo} alt='Store Logo' />}
       </Col>
-      <Col span={12} className='total'>{displayTotal ? `Total: ${currency}${totalPrice(cartContents)}` : <Search
+      <Col span={12} className='total'>{displayTotal ? `Total: ${sign}${totalPrice(cartContents)}` : <Search
         onChange={change}
         placeholder='search'
       />}
---
+++ src/components/singleProduct/index.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
+import '../../less/index.less'
+import CartHeader from '../elements/cartHeader'
+import SingleProductView from './buyerSingleProductView'
+
+function Single (props) {
+  const productId = props.match.params.id
+  const [up, setUp] = useState(false)
+  const cartContents = useSelector(state => state.cart)
+  const store = useSelector(state => state.user)
+  var lastScrollTop = 0
+
+  window.addEventListener('scroll', function () {
+    var st = window.pageYOffset || document.documentElement.scrollTop
+    if (st > lastScrollTop) {
+      setUp(true)
+    } else {
+      setUp(false)
+    }
+    lastScrollTop = st <= 0 ? 0 : st
+  }, false)
+  return (
+    <div>
+      <CartHeader top={up} displayTotal badgeCount={cartContents.length} logoPath={store.user.imageUrl} displayBack={props.history.goBack} />
+      <SingleProductView productId={productId} />
+    </div>
+  )
+}
+
+export default Single
