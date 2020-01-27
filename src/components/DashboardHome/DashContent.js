800df8a2048c4d2ff3eaf45a3ee19d678f22109d
Shaun Orpen <shaunorpen@users.noreply.github.com>
2020-01-27 10:27:53 +0000

    Add currency symbol to dashboard view

--- src/components/DashboardHome/DashContent.js
+++ src/components/DashboardHome/DashContent.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Tabs } from 'antd'
 import './Dashboard.css'
 import Pane1 from './Pane1'
@@ -7,11 +7,30 @@ import Pane2 from './Pane2'
 const { TabPane } = Tabs
 
 const Content = ({ currency }) => {
+
+  const [sign, setSign] = useState('')
+  const fixCurrency = (currency) => {
+    if (currency === 'POU') {
+      setSign('£')
+    } else if (currency === 'DOL') {
+      setSign('$')
+    } else if (currency === 'EUR') {
+      setSign('€')
+    } else if (currency === 'YEN') {
+      setSign('¥')
+    } else {
+      return undefined
+    }
+  }
+  useEffect(() => {
+    fixCurrency(currency)
+  }, [currency])
+
   return (
     <div>
       <Tabs defaultActiveKey='1' className='content'>
         <TabPane tab='Overview' key='1'>
-          <Pane1 currency={currency} />
+          <Pane1 currency={sign} />
         </TabPane>
         <TabPane tab='Sales History' key='2'>
       Today
