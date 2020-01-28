e1ffdc53295f1b138aa62ad5ce046f4ef61f55dd
aapetsi <apetsiampiah@gmail.com>
2020-01-28 11:37:22 +0000

    ft: populate dashboard with transaction history

--- src/components/DashboardHome/Pane1.js
+++ src/components/DashboardHome/Pane1.js
@@ -3,15 +3,33 @@ import React from 'react'
 const Pane1 = ({ currency }) => {
   return (
     <div>
-      <h2 style={{ textAlign: 'left', marginLeft: '40px', paddingTop: '20px' }}>Earnings</h2>
+      <h2 style={{ textAlign: 'left', marginLeft: '40px', paddingTop: '20px' }}>
+        Earnings
+      </h2>
       <div id='cards'>
         <div className='Cards'>
-          <p style={{ color: 'white', paddingTop: '20px' }}>This month you earned:</p>
-          <p className='earnings'>0<span className='currency'> {currency}</span></p>
+          <p style={{ color: 'white', paddingTop: '20px' }}>
+            This month you earned:
+          </p>
+          <p className='earnings'>
+            0<span className='currency'> {currency}</span>
+          </p>
         </div>
         <div className='Cards2'>
-          <p style={{ color: 'white', paddingTop: '20px' }}>Lifetime earnings:</p>
-          <p className='earnings'>0<span className='currency'> {currency}</span></p>
+          <p style={{ color: 'white', paddingTop: '20px' }}>
+            Lifetime earnings:
+          </p>
+          <p className='earnings'>
+            0<span className='currency'> {currency}</span>
+          </p>
+        </div>
+        <div className='Cards2'>
+          <p style={{ color: 'white', paddingTop: '20px' }}>
+            Lifetime earnings:
+          </p>
+          <p className='earnings'>
+            0<span className='currency'> {currency}</span>
+          </p>
         </div>
       </div>
     </div>
