17497f939f5a7ad01049779df6fd68722ecab7db
Rexben001 <rexben.rb@gmail.com>
2020-01-28 21:06:13 +0100

    add the total sales, name, pricr, desc

--- src/components/DashboardHome/Pane2.js
+++ src/components/DashboardHome/Pane2.js
@@ -1,23 +1,24 @@
-import React from 'react'
-import './Dashboard.css'
-import { Icon } from 'antd'
+import React from "react";
+import "./Dashboard.css";
+import { Icon } from "antd";
 
-const Pane2 = () => {
+const Pane2 = ({ name, description, price ,checkoutDate}) => {
+  console.log(name, description, price, checkoutDate);
   return (
-    <div className='Card2'>
-      <div className='PaneFlex'>
-        <Icon type='check-circle' style={{ color: '#BFD7EA' }} />
-        <div className='StoreTitle'>
-          <h2>Calvin Klein Boxers Shorts</h2>
-          <p>Fine linen, rare edition, worn by Bieber</p>
+    <div className="Card2">
+      <div className="PaneFlex">
+        <Icon type="check-circle" style={{ color: "#BFD7EA" }} />
+        <div className="StoreTitle">
+          <h2>{name}</h2>
+          <p>{description}</p>
         </div>
-        <div className='StorePrice'>
-          <h2>+₦500</h2>
+        <div className="StorePrice">
+          <h2>{price}</h2>
           <p>Dec. 14</p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Pane2
+export default Pane2;
