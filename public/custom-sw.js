73cef242abb3883d77bf92fe581528f0d7a7cfef
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2020-01-29 04:02:02 +0100

    ch: change serviceworker origin

--- public/custom-sw.js
+++ public/custom-sw.js
@@ -1,45 +1,56 @@
-//var CACHE_NAME = 'pwa-task-manager';
+var CACHE_NAME = 'pwa-task-manager';
 var urlsToCache = [
-  '/',
-  './manifest.json',
-  './index.html'
+  './',
+  './dashboard',
+  './inventory',
+  './account',
+  './profile'
 ];
 
 // Install a service worker
-self.addEventListener('install', async event => {
+self.addEventListener('install', event => {
   // Perform install steps
-  self.skipWaiting();
-  const cache = await caches.open('app-static');
-  cache.addAll(urlsToCache);
-
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
 // Cache and return requests
-self.addEventListener('fetch', event => {
- const req = event.request;
- const url = new URL(req.url)
- if(url.origin === location.origin){
-   event.respondWith(cacheFirst(req))
- }else{
-   event.respondWith(networkFirst(req));
- }
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.open("pure-dynamic").then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        // Cache hit - return response
+        return (
+          response ||
+          fetch(event.request).then(function(response) {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(err=>{
+            return cache.match(event.request)
+          })
+        );
+      });
+    })
+  );
 });
 
 // Update a service worker
-self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
-
-async function cacheFirst(req){
-  const cachedResponse = await caches.match(req);
-  return cachedResponse || fetch(req)
-}
-
-async function networkFirst(req){
-  const cache = await caches.open('app-dynamic');
-  try {
-    const res = await fetch(req);
-    cache.put(req, res.clone());
-    return res;
-  } catch (error) {
-    return await cache.match(req)
-  }
-}
+self.addEventListener("activate", event => {
+  var cacheWhitelist = ["pwa-task-manager"];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
