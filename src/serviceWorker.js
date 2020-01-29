d0a01a709dc40f6e182ce1fde097696aa150bed2
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2020-01-29 12:37:38 +0100

    ft: revert to caching of static assests

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -32,7 +32,7 @@ export function register (config) {
     }
 
     window.addEventListener('load', () => {
-      const swUrl = `${process.env.PUBLIC_URL}/custom-sw.js`
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
@@ -97,15 +97,6 @@ function registerValidSW (swUrl, config) {
       console.error('Error during service worker registration:', error)
     })
 }
-var refreshing;
-navigator.serviceWorker.addEventListener('controllerchange', () => {
-  if (refreshing) {
-    return;
-  }
-
-  refreshing = true;
-  window.location.reload();
-});
 
 function checkValidServiceWorker (swUrl, config) {
   // Check if the service worker can be found. If it can't reload the page.
