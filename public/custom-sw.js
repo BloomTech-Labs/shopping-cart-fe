3390c99ff5d7717f28f9b4eb56fe2727a56878cc
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2020-01-29 02:44:17 +0100

    ch: clean up code

--- public/custom-sw.js
+++ public/custom-sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'pwa-task-manager';
+//var CACHE_NAME = 'pwa-task-manager';
 var urlsToCache = [
   '/',
   './manifest.json',
@@ -6,46 +6,40 @@ var urlsToCache = [
 ];
 
 // Install a service worker
-self.addEventListener('install', event => {
+self.addEventListener('install', async event => {
   // Perform install steps
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+  const cache = await caches.open('app-static');
+  cache.addAll(urlsToCache);
+
 });
 
 // Cache and return requests
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+ const req = event.request;
+ const url = new URL(req.url)
+ if(url.origin === location.origin){
+   event.respondWith(cacheFirst(req))
+ }else{
+   event.respondWith(networkFirst(req));
+ }
 });
 
 // Update a service worker
-self.addEventListener('activate', event => {
-  var cacheWhitelist = ['pwa-task-manager'];
-  self.clients.claim();
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+
+async function cacheFirst(req){
+  const cachedResponse = await caches.match(req);
+  return cachedResponse || fetch(req)
+}
+
+async function networkFirst(req){
+  const cache = await caches.open('app-dynamic');
+  try {
+    const res = await fetch(req);
+    cache.put(req, res.clone());
+    return res;
+  } catch (error) {
+    return await cache.match(req)
+  }
+}
