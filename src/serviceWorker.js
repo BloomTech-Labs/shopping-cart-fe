e778c2b16f3d51393abb6340fcf8125315502d79
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2020-01-29 12:35:16 +0100

    bg: fix bug

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -97,6 +97,15 @@ function registerValidSW (swUrl, config) {
       console.error('Error during service worker registration:', error)
     })
 }
+var refreshing;
+navigator.serviceWorker.addEventListener('controllerchange', () => {
+  if (refreshing) {
+    return;
+  }
+
+  refreshing = true;
+  window.location.reload();
+});
 
 function checkValidServiceWorker (swUrl, config) {
   // Check if the service worker can be found. If it can't reload the page.
