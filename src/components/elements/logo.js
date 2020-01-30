5e652519163ab9ccd7b389685516504a24ade96e
Tolu Atolagbe <tolu.atolagbe@gmail.com>
2020-01-30 15:05:41 +0100

    ch: run standard to conform with styling

--- src/components/elements/logo.js
+++ src/components/elements/logo.js
@@ -1,14 +1,14 @@
-import React from "react";
-import logo from "../../images/PureRetail_Logo.png";
-import { useSelector } from "react-redux";
+import React from 'react'
+import logo from '../../images/PureRetail_Logo.png'
+import { useSelector } from 'react-redux'
 
 const Logo = () => {
-  const {user} = useSelector(state => state.user);
+  const { user } = useSelector(state => state.user)
   return (
-    <div id="logo">
-      <img src={user.imageUrl || logo} alt="PureRetail Logo" />
+    <div id='logo'>
+      <img src={user.imageUrl || logo} alt='PureRetail Logo' />
     </div>
-  );
-};
+  )
+}
 
-export default Logo;
+export default Logo
--- src/components/support.js
+++ src/components/support.js
@@ -3,21 +3,22 @@ import { Link } from 'react-router-dom'
 import Logo from './elements/logo'
 
 const Support = () => {
-    return (
-        <div style={{
-            padding: '3rem', 
-            textAlign: 'center',
-        }}>
-            <Logo />
-            <div>
-                <h2>Support</h2>
-                <p style={{ margin: '5rem 0rem', fontSize: 'medium'}}>Please contact<br/>pureretail12345@gmail.com<br/>for support.</p>
-            </div>
-            <div style={{ fontSize: 'medium', marginTop: '20rem'}}>
-                <Link to='/'>Back to Login</Link>
-            </div>
+  return (
+    <div style={{
+      padding: '3rem',
+      textAlign: 'center'
+    }}
+    >
+      <Logo />
+      <div>
+        <h2>Support</h2>
+        <p style={{ margin: '5rem 0rem', fontSize: 'medium' }}>Please contact<br />pureretail12345@gmail.com<br />for support.</p>
       </div>
-    )
+      <div style={{ fontSize: 'medium', marginTop: '20rem' }}>
+        <Link to='/'>Back to Login</Link>
+      </div>
+    </div>
+  )
 }
 
 export default Support
