279798a83d9bdc888bc677a1d7ff41b62f12894b
chirag <harithesia@gmail.com>
2020-04-27 18:14:30 -0500

    Changed the API URL to LabsPT10 Heroku test-api/heroku app

--- src/components/ResetPassword/setNewPassword.js
+++ src/components/ResetPassword/setNewPassword.js
@@ -1,174 +1,170 @@
-import React, { useState } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import { Form, Input, Icon, Button, message, Spin, Modal } from 'antd'
-import { useSelector, useDispatch } from 'react-redux'
-import Logo from '../elements/logo'
-import history from '../../history'
-import * as creators from '../../state/actionCreators'
-import image from '../../images/security.png'
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { Form, Input, Icon, Button, message, Spin, Modal } from 'antd';
+import { useSelector, useDispatch } from 'react-redux';
+import Logo from '../elements/logo';
+import history from '../../history';
+import * as creators from '../../state/actionCreators';
+import image from '../../images/security.png';
 
-const SetNewPassword = props => {
-  const dispatch = useDispatch()
-  const { isLoading } = useSelector(state => state.user)
+const SetNewPassword = (props) => {
+	const dispatch = useDispatch();
+	const { isLoading } = useSelector((state) => state.user);
 
-  const [confirmDirty, setConfirmDirty] = useState(false)
-  const token = window.location.href.split('=')[1]
-  const URL = `https://shopping-cart-eu3.herokuapp.com/api/auth/reset/${token}`
-  const handleSubmit = e => {
-    e.preventDefault()
-    props.form.validateFieldsAndScroll((err, values) => {
-      const payload = {
-        password: values.password
-      }
-      if (!err) {
-        dispatch(creators.setLoading(true))
-        axios
-          .post(URL, payload)
-          .then(res => {
-            dispatch(creators.setLoading(false))
-            dispatch(creators.clearErrors())
-            Modal.info({
-              title: 'Success',
-              content: 'Your password has been reset successfully.',
-              centered: true,
-              onOk () {
-                history.push('/')
-              }
-            })
-          })
-          .catch(error => {
-            dispatch(creators.setLoading(false))
-            dispatch(creators.setErrors(error.response.data))
-            message.error(Object.values(error.response.data)[0])
-          })
-      } else {
-        message.error('Password validation failed.')
-      }
-    })
-  }
-  const handleConfirmBlur = e => {
-    const { value } = e.target
-    setConfirmDirty(!!value)
-  }
-  const compareToFirstPassword = (rule, value, callback) => {
-    const { form } = props
-    if (value && value !== form.getFieldValue('password')) {
-      callback('The two passwords that you entered do not match!')
-    } else {
-      callback()
-    }
-  }
-  const validateToNextPassword = (rule, value, callback) => {
-    const { form } = props
-    if (value && confirmDirty) {
-      form.validateFields(['confirm'], { force: true })
-    }
-    callback()
-  }
+	const [ confirmDirty, setConfirmDirty ] = useState(false);
+	const token = window.location.href.split('=')[1];
+	const URL = `https://whispering-dawn-20611.herokuapp.com/api/auth/reset/${token}`;
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		props.form.validateFieldsAndScroll((err, values) => {
+			const payload = {
+				password: values.password
+			};
+			if (!err) {
+				dispatch(creators.setLoading(true));
+				axios
+					.post(URL, payload)
+					.then((res) => {
+						dispatch(creators.setLoading(false));
+						dispatch(creators.clearErrors());
+						Modal.info({
+							title: 'Success',
+							content: 'Your password has been reset successfully.',
+							centered: true,
+							onOk() {
+								history.push('/');
+							}
+						});
+					})
+					.catch((error) => {
+						dispatch(creators.setLoading(false));
+						dispatch(creators.setErrors(error.response.data));
+						message.error(Object.values(error.response.data)[0]);
+					});
+			}
+			else {
+				message.error('Password validation failed.');
+			}
+		});
+	};
+	const handleConfirmBlur = (e) => {
+		const { value } = e.target;
+		setConfirmDirty(!!value);
+	};
+	const compareToFirstPassword = (rule, value, callback) => {
+		const { form } = props;
+		if (value && value !== form.getFieldValue('password')) {
+			callback('The two passwords that you entered do not match!');
+		}
+		else {
+			callback();
+		}
+	};
+	const validateToNextPassword = (rule, value, callback) => {
+		const { form } = props;
+		if (value && confirmDirty) {
+			form.validateFields([ 'confirm' ], { force: true });
+		}
+		callback();
+	};
 
-  const { getFieldDecorator } = props.form
+	const { getFieldDecorator } = props.form;
 
-  const formItemLayout = {
-    labelCol: {
-      xs: { span: 24 },
-      sm: { span: 8 }
-    },
-    wrapperCol: {
-      xs: { span: 24 },
-      sm: { span: 16 }
-    }
-  }
-  const tailFormItemLayout = {
-    wrapperCol: {
-      xs: {
-        span: 24,
-        offset: 0
-      },
-      sm: {
-        span: 16,
-        offset: 8
-      }
-    }
-  }
+	const formItemLayout = {
+		labelCol: {
+			xs: { span: 24 },
+			sm: { span: 8 }
+		},
+		wrapperCol: {
+			xs: { span: 24 },
+			sm: { span: 16 }
+		}
+	};
+	const tailFormItemLayout = {
+		wrapperCol: {
+			xs: {
+				span: 24,
+				offset: 0
+			},
+			sm: {
+				span: 16,
+				offset: 8
+			}
+		}
+	};
 
-  const setNewPasswordForm = (
-    <Spin spinning={isLoading}>
-      <div className='cover'>
-        <div className='desktop-logo'>
-          <h2 className='reset-password-text'>Set new password</h2>
-          <div className='desktop-logo-large'>
-            <img src={image} alt='PureRetail Logo' width='372' height='372' />
-          </div>
-        </div>
-        <Logo />
-        <div className='desktop-form'>
-          <Form {...formItemLayout} onSubmit={handleSubmit}>
-            <div id='header'>
-              <h2>Set new password</h2>
-            </div>
-            <Form.Item hasFeedback>
-              {getFieldDecorator('password', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please input your new password!'
-                  },
-                  {
-                    validator: validateToNextPassword
-                  }
-                ]
-              })(
-                <Input.Password
-                  placeholder='New Password'
-                  prefix={
-                    <Icon type='lock' style={{ color: 'rgba(0,0,0,.70)' }} />
-                  }
-                />
-              )}
-            </Form.Item>
-            <Form.Item hasFeedback>
-              {getFieldDecorator('confirm', {
-                rules: [
-                  {
-                    required: true,
-                    message: 'Please confirm your new password!'
-                  },
-                  {
-                    validator: compareToFirstPassword
-                  }
-                ]
-              })(
-                <Input.Password
-                  onBlur={handleConfirmBlur}
-                  placeholder='Confirm New Password'
-                  prefix={
-                    <Icon type='lock' style={{ color: 'rgba(0,0,0,.70)' }} />
-                  }
-                />
-              )}
-            </Form.Item>
-            <Form.Item {...tailFormItemLayout}>
-              <Button type='primary' htmlType='submit'>
-                Reset
-              </Button>
-            </Form.Item>
-          </Form>
-          <div id='or_login'>
-            <p>
-              or <Link to='/'>login</Link> instead
-            </p>
-          </div>
-        </div>
-      </div>
-    </Spin>
-  )
+	const setNewPasswordForm = (
+		<Spin spinning={isLoading}>
+			<div className="cover">
+				<div className="desktop-logo">
+					<h2 className="reset-password-text">Set new password</h2>
+					<div className="desktop-logo-large">
+						<img src={image} alt="PureRetail Logo" width="372" height="372" />
+					</div>
+				</div>
+				<Logo />
+				<div className="desktop-form">
+					<Form {...formItemLayout} onSubmit={handleSubmit}>
+						<div id="header">
+							<h2>Set new password</h2>
+						</div>
+						<Form.Item hasFeedback>
+							{getFieldDecorator('password', {
+								rules: [
+									{
+										required: true,
+										message: 'Please input your new password!'
+									},
+									{
+										validator: validateToNextPassword
+									}
+								]
+							})(
+								<Input.Password
+									placeholder="New Password"
+									prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.70)' }} />}
+								/>
+							)}
+						</Form.Item>
+						<Form.Item hasFeedback>
+							{getFieldDecorator('confirm', {
+								rules: [
+									{
+										required: true,
+										message: 'Please confirm your new password!'
+									},
+									{
+										validator: compareToFirstPassword
+									}
+								]
+							})(
+								<Input.Password
+									onBlur={handleConfirmBlur}
+									placeholder="Confirm New Password"
+									prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.70)' }} />}
+								/>
+							)}
+						</Form.Item>
+						<Form.Item {...tailFormItemLayout}>
+							<Button type="primary" htmlType="submit">
+								Reset
+							</Button>
+						</Form.Item>
+					</Form>
+					<div id="or_login">
+						<p>
+							or <Link to="/">login</Link> instead
+						</p>
+					</div>
+				</div>
+			</div>
+		</Spin>
+	);
 
-  return setNewPasswordForm
-}
+	return setNewPasswordForm;
+};
 
-const SetNewPasswordForm = Form.create({ name: 'setNewPassword' })(
-  SetNewPassword
-)
+const SetNewPasswordForm = Form.create({ name: 'setNewPassword' })(SetNewPassword);
 
-export default SetNewPasswordForm
+export default SetNewPasswordForm;
--- src/components/SellerAccount/SellerAccount.js
+++ src/components/SellerAccount/SellerAccount.js
@@ -1,61 +1,55 @@
-import React, { useState, useEffect } from 'react'
-import { Card, Button } from 'antd'
-import axios from 'axios'
-import Nav from '../elements/nav'
-import withAuth from '../Auth/axiosWithAuth'
+import React, { useState, useEffect } from 'react';
+import { Card, Button } from 'antd';
+import axios from 'axios';
+import Nav from '../elements/nav';
+import withAuth from '../Auth/axiosWithAuth';
 
-const storeURL = 'https://shopping-cart-eu3.herokuapp.com/api/store'
-const stripeURL = 'https://shopping-cart-eu3.herokuapp.com/api/auth/stripe'
+const storeURL = 'https://whispering-dawn-20611.herokuapp.com/api/store';
+const stripeURL = 'https://whispering-dawn-20611.herokuapp.com/api/auth/stripe';
 
-function Account () {
-  const [stripeId, setStripeId] = useState('')
-  const [storeId, setStoreId] = useState('')
-  useEffect(() => {
-    withAuth()
-      .get(storeURL)
-      .then(res => {
-        setStoreId(res.data._id)
-        setStripeId(res.data.stripeId)
-      })
-      .catch(error => {
-        console.log(error)
-      })
-  }, [])
+function Account() {
+	const [ stripeId, setStripeId ] = useState('');
+	const [ storeId, setStoreId ] = useState('');
+	useEffect(() => {
+		withAuth()
+			.get(storeURL)
+			.then((res) => {
+				setStoreId(res.data._id);
+				setStripeId(res.data.stripeId);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	}, []);
 
-  const connectStripe = e => {
-    e.preventDefault()
-    axios
-      .post(stripeURL, { storeId })
-      .then(res => {
-        window.location.href = stripeURL
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+	const connectStripe = (e) => {
+		e.preventDefault();
+		axios
+			.post(stripeURL, { storeId })
+			.then((res) => {
+				window.location.href = stripeURL;
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
 
-  return (
-    <div className='seller-account'>
-      <div className='main'>
-        <h2>Account</h2>
-        <Card
-          className='Card'
-          title='Your Stripe ID'
-          style={{ fontWeight: '900' }}
-        >
-          <p id='stripeID'>
-            {stripeId || 'Your stripe account is not connected'}
-          </p>
-        </Card>
-        {stripeId ? (
-          <Button onClick={connectStripe}>Change Stripe Id</Button>
-        ) : (
-          <Button onClick={connectStripe}>Connect to Stripe</Button>
-        )}
-      </div>
-      <Nav />
-    </div>
-  )
+	return (
+		<div className="seller-account">
+			<div className="main">
+				<h2>Account</h2>
+				<Card className="Card" title="Your Stripe ID" style={{ fontWeight: '900' }}>
+					<p id="stripeID">{stripeId || 'Your stripe account is not connected'}</p>
+				</Card>
+				{stripeId ? (
+					<Button onClick={connectStripe}>Change Stripe Id</Button>
+				) : (
+					<Button onClick={connectStripe}>Connect to Stripe</Button>
+				)}
+			</div>
+			<Nav />
+		</div>
+	);
 }
 
-export default Account
+export default Account;
