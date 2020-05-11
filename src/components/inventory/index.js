6526dbb4950885b724cd662719fe6af02e0c2c64
mjedwards <jairedwards20@gmail.com>
2020-05-11 19:31:38 -0400

    made minor adjustments

--- src/components/inventory/index.js
+++ src/components/inventory/index.js
@@ -1,6 +1,5 @@
 import React from "react";
-// import Inventory from "./inventory";
-import Inventory from "./_inventory";
+import Inventory from "./inventory";
 import Navbar from "../Navbar";
 // import Nav from '../elements/nav'
 // import Footer from '../DashboardHome/Footer'
