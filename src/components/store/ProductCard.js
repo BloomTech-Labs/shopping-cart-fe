22a63d36df5dd5632fff8dd51873c534d98ca7f4
Matt Vaccaro <mattvaccaro@Matts-MBP.hsd1.tn.comcast.net>
2020-05-21 19:38:58 -0500

    making progress

---
+++ src/components/store/ProductCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const ProductCard = (props) => {
+	const { images, name, price, _id } = props.inventory;
+	return (
+		<div className="productCardContainer">
+			<NavLink to={`/product/${_id}`}>
+				<img className="productImage" src={images[0]} />
+
+				<p>{name}</p>
+				<h3>{price}</h3>
+			</NavLink>
+		</div>
+	);
+};
+
+export default ProductCard;
