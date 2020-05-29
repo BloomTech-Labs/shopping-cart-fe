96ec0b6a496c9f892d840dd67d3fd2778505ae75
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-05-28 21:39:18 -0400

    initial commit

---
+++ src/components/categoryPicker.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CategoryPicker = () => {
+
+    const categories = ["Shirt", "Shoes", "Shoes", "Shorts", "Jeans", "Jeans"];
+    const uniqueCategories = [];
+
+
+
+  return (
+    <div>
+      <h1>Is this rendering?</h1>
+    </div>
+  );
+};
+
+export default CategoryPicker;
