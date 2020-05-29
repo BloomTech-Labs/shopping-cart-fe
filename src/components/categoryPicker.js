fe15add00aa876b6713de48b1e8be7eb80ffd364
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-05-29 19:17:16 -0400

    greatness about to ensue

--- src/components/categoryPicker.js
+++ src/components/categoryPicker.js
@@ -1,15 +1,26 @@
 import React from 'react';
+// the route is '/category'
 
 const CategoryPicker = () => {
-
-    const categories = ["Shirt", "Shoes", "Shoes", "Shorts", "Jeans", "Jeans"];
-    const uniqueCategories = [];
-
-
+  const categories = [
+    'Shirt',
+    'Shoes',
+    'Shoes',
+    'Shoes',
+    'Shorts',
+    'Jeans',
+    'Jeans',
+  ];
+  const uniqueCategories = [...new Set(categories)];
+  console.log(uniqueCategories);
 
   return (
     <div>
       <h1>Is this rendering?</h1>
+      <button onClick={console.log('all of them')}>All</button>
+      {uniqueCategories.map((item) => {
+        return <button onClick={console.log('button clicked')}>{item}</button>;
+      })}
     </div>
   );
 };
