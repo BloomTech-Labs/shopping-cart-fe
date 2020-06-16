d3ef11ec39230e85e7f252307193157ff2efa634
Matt Vaccaro <mattvaccaro@Matts-MBP.hsd1.tn.comcast.net>
2020-06-15 19:18:47 -0500

    fixing errors

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ import OrderDetailsView from "./components/Orders/OrderDetailsView"
 function App() {
   window.addEventListener('load', () => {
     function handleNetworkChange(event) {
-      
+      // delete this comment
       if (navigator.onLine) {
         document.getElementById('offline-notification').style.display = 'none';
       } else {
