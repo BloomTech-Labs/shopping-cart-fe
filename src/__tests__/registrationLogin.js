737b71e455406539f75b1bec8bf643e3c94db1b6
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-06-18 21:03:55 -0400

    login screen rendering tests written for each element

--- src/__tests__/registrationLogin.js
+++ src/__tests__/registrationLogin.js
@@ -2,12 +2,52 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import renderWithProviders from '../utlities/renderWithProviders';
 import Login from '../components/login';
-import { getByLabelText } from '@testing-library/react';
+import { getByTestId, getByAltText } from '@testing-library/react';
+
+// These tests ensure all elements are rendering on login / registration screens
 
 describe('login screen and registration screens', () => {
   test('renders the title on login', () => {
     const { getByText } = renderWithProviders(<Login />);
-    const title = getByText(/Login to your Store/i);
+    const title = getByText(/LogIn to your Store/i);
     expect(title).toBeVisible();
   });
+  test('renders logo', () => {
+    const { getByTestId } = renderWithProviders(<Login />);
+    expect(getByTestId('loginLogo')).toBeVisible();
+  });
+  test('renders phone number input', () => {
+    const { getByTestId } = renderWithProviders(<Login />);
+    expect(getByTestId('phoneNumberInput')).toBeVisible();
+  });
+  test('renders password input', () => {
+    const { getByTestId } = renderWithProviders(<Login />);
+    expect(getByTestId('loginPasswordInput')).toBeVisible();
+  });
+  test('renders forgot password link', () => {
+    const { getByText } = renderWithProviders(<Login />);
+    expect(getByText(/forgot password/i)).toBeVisible();
+  });
+  test('renders login button', () => {
+    const { getByTestId } = renderWithProviders(<Login />);
+    expect(getByTestId('loginButton')).toBeVisible();
+  });
+  test('renders create store button', () => {
+    const { getByTestId } = renderWithProviders(<Login />);
+    expect(getByTestId('createStoreButton')).toBeVisible();
+  });
+  test('renders main login image', () => {
+    const { getByAltText } = renderWithProviders(<Login />);
+    expect(getByAltText(/login image/i)).toBeVisible();
+  });
+  test('renders colorful image background', () => {
+    const { getByTestId } = renderWithProviders(<Login />);
+    expect(getByTestId('imageBackground')).toBeVisible();
+  });
+  test('renders phone / password background div', () => {
+    const { getByTestId } = renderWithProviders(<Login />);
+    expect(getByTestId('loginFormWrapper')).toBeVisible();
+  });
+  // 'Create Store Screen' testing starts here
+  
 });
