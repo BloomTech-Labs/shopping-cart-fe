592dc9d798d5bec243af3b499fefb2c8c9448121
mjedwards <jairedwards20@gmail.com>
2020-06-22 20:55:12 -0400

    tests were created to make sure component is visible

--- src/__tests__/DashboardHome/Dashboard.js
+++ src/__tests__/DashboardHome/Dashboard.js
@@ -1,38 +1,40 @@
-import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
-import renderWithProviders from '../../utlities/renderWithProviders'
+import React from "react";
+import { fireEvent, waitForElement } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import renderWithProviders from "../../utlities/renderWithProviders";
 
-import Container from '../../components/DashboardHome/Dashboard'
+import Container from "../../components/Orders/Orders";
+import OrderContainer from "../../components/Orders/OrderDetailsView";
 
-describe('dashboard', () => {
-  test('renders the welcome message', () => {
-    const { getByText } = renderWithProviders(<Container />)
-    const element = getByText(/welcome/i)
-    expect(element).toBeVisible()
-  })
-  test('renders the logo', () => {
-    const { getByAltText } = renderWithProviders(<Container />)
-    const element = getByAltText(/store logo/i)
-    expect(element).toBeVisible()
-  })
-  test('renders overview', () => {
-    const { getByText } = renderWithProviders(<Container />)
-    const element = getByText(/overview/i)
-    expect(element).toBeVisible()
-  })
-  test('renders sales history', () => {
-    const { getByText } = renderWithProviders(<Container />)
-    const element = getByText(/sales history/i)
-    expect(element).toBeVisible()
-  })
-  test('renders this month you earned', () => {
-    const { getByText } = renderWithProviders(<Container />)
-    const element = getByText(/this month you earned/i)
-    expect(element).toBeVisible()
-  })
-  test('renders lifetime earnings', () => {
-    const { getByText } = renderWithProviders(<Container />)
-    const element = getByText(/lifetime earnings/i)
-    expect(element).toBeVisible()
-  })
-})
+describe("dashboard", () => {
+  test("renders the 'Order #' title", () => {
+    const { getByText } = renderWithProviders(<Container />);
+    const element = getByText(/Order #/i);
+    expect(element).toBeVisible();
+  });
+  test("renders the 'Customer Name' title", () => {
+    const { getByText } = renderWithProviders(<Container />);
+    const element = getByText(/Customer Name/i);
+    expect(element).toBeVisible();
+  });
+  test("renders the 'Total Items' title", () => {
+    const { getByText } = renderWithProviders(<Container />);
+    const element = getByText(/Total Items/i);
+    expect(element).toBeVisible();
+  });
+  test("renders the 'Status' title", () => {
+    const { getByText } = renderWithProviders(<Container />);
+    const element = getByText(/Status/i);
+    expect(element).toBeVisible();
+  });
+  test("renders the 'Date' title", () => {
+    const { getByText } = renderWithProviders(<Container />);
+    const element = getByText(/Date/i);
+    expect(element).toBeVisible();
+  });
+  test("renders the 'Actions' title", () => {
+    const { getByText } = renderWithProviders(<Container />);
+    const element = getByText(/Actions/i);
+    expect(element).toBeVisible();
+  });
+});
