2daf293267fd020e2342e7db14387dbbece0700b
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2020-06-23 19:15:46 -0400

    fixed

--- src/components/register.js
+++ src/components/register.js
@@ -1,65 +1,65 @@
-import React, { useState } from "react"
-import axios from "axios"
-import { Link } from "react-router-dom"
-import { Form, Input, Icon, Button, message, Spin } from "antd"
+import React, { useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { Form, Input, Icon, Button, message, Spin } from 'antd';
+import Logo from './elements/logo';
+import image from '../images/register.png';
+import history from '../history';
+import { setLoading, setErrors, clearErrors } from '../state/actionCreators';
+import { connect } from 'react-redux';
 
-import image from "../images/register.png"
-import history from "../history"
-import { setLoading, setErrors, clearErrors } from "../state/actionCreators"
-import { connect } from "react-redux"
-
-const signupURL = "https://shopping-cart-be.herokuapp.com/api/auth/register"
+const signupURL = 'https://shopping-cart-be.herokuapp.com/api/auth/register';
 const RegistrationForm = (props) => {
-  const [confirmDirty, setConfirmDirty] = useState(false)
+  const [confirmDirty, setConfirmDirty] = useState(false);
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     props.form.validateFieldsAndScroll((err, values) => {
       const payload = {
         phone: values.number.trim(),
         password: values.password,
-      }
+      };
       if (!err) {
-        props.dispatch(setLoading(true))
+        props.dispatch(setLoading(true));
         axios
           .post(signupURL, payload)
           .then((res) => {
-            message.success("Signed Up")
-            localStorage.setItem("token", res.data.token)
-            props.dispatch(clearErrors())
-            history.push("/welcome")
+            message.success('Signed Up');
+            localStorage.setItem('token', res.data.token);
+            props.dispatch(clearErrors());
+            history.push('/welcome');
           })
           .catch((error) => {
-            props.dispatch(setErrors(error.response.data))
-            props.dispatch(setLoading(false))
-            message.error(Object.values(error.response.data)[0])
-          })
+            props.dispatch(setErrors(error.response.data));
+            props.dispatch(setLoading(false));
+            message.error(Object.values(error.response.data)[0]);
+          });
       } else {
-        message.error("Enter Required Fields")
+        message.error('Enter Required Fields');
       }
-    })
-  }
+    });
+  };
   const handleConfirmBlur = (e) => {
-    const { value } = e.target
-    setConfirmDirty(!!value)
-  }
+    const { value } = e.target;
+    setConfirmDirty(!!value);
+  };
   const compareToFirstPassword = (rule, value, callback) => {
-    const { form } = props
-    if (value && value !== form.getFieldValue("password")) {
-      callback("Passwords do not match!")
+    const { form } = props;
+    if (value && value !== form.getFieldValue('password')) {
+      callback('Passwords do not match!');
     } else {
-      callback()
+      callback();
     }
-  }
+  };
   const validateToNextPassword = (rule, value, callback) => {
-    const { form } = props
+    const { form } = props;
 
     if (value && confirmDirty) {
-      form.validateFields(["confirm"], { force: true })
+      form.validateFields(['confirm'], { force: true });
     }
-    callback()
-  }
+    callback();
+  };
 
-  const { getFieldDecorator } = props.form
+  const { getFieldDecorator } = props.form;
 
   const formItemLayout = {
     labelCol: {
@@ -70,7 +70,7 @@ const RegistrationForm = (props) => {
       xs: { span: 24 },
       sm: { span: 16 },
     },
-  }
+  };
   const tailFormItemLayout = {
     wrapperCol: {
       xs: {
@@ -82,7 +82,7 @@ const RegistrationForm = (props) => {
         offset: 8,
       },
     },
-  }
+  };
 
   const registerForm = (
     <Spin spinning={props.isLoading}>
@@ -90,10 +90,10 @@ const RegistrationForm = (props) => {
         <div className='desktop-logo' data-testid='desktopLogoDiv'>
           <div className='desktop-logo-large'>
             <img
-              src="https://images.unsplash.com/photo-1556741533-6e6a62bd8b49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-              alt="Register"
-              height="400"
-              className="loginPicture"
+              src='https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+              alt='Registration'
+              height='400'
+              className='loginPicture'
             />
           </div>
         </div>
@@ -102,40 +102,38 @@ const RegistrationForm = (props) => {
 
         <div className='desktop-form' data-testid='registrationFormDiv'>
           <Form {...formItemLayout} onSubmit={handleSubmit}>
-            <img
-              className="pureRetailImage"
-              src="pureRetail-2020-logo.svg"
-              alt="pure retail logo"
-            />
-                <h4 className='loginHeader'>Register New Account!</h4>
-            <div id="header"></div>
             <Form.Item>
-              {getFieldDecorator("number", {
+              <img
+                className='pureRetailImage registrationHeader'
+                src='pureRetail-2020-logo.svg'
+                alt='pure retail logo'
+              />
+              {getFieldDecorator('number', {
                 rules: [
                   {
-                    message: "Enter valid Whatsapp number",
+                    message: 'Enter valid Whatsapp number',
                   },
                   {
                     required: true,
-                    message: "Enter valid Whatsapp number",
+                    message: 'Enter valid Whatsapp number',
                   },
                 ],
               })(
                 <Input
-                  className="form-input"
-                  placeholder="e.g. 2348000001231"
+                  className='input'
+                  placeholder='e.g. 2348000001231'
                   prefix={
-                    <Icon type="phone" style={{ color: "rgba(0,0,0,.70)" }} />
+                    <Icon type='phone' style={{ color: 'rgba(0,0,0,.70)' }} />
                   }
                 />
               )}
             </Form.Item>
             <Form.Item hasFeedback>
-              {getFieldDecorator("password", {
+              {getFieldDecorator('password', {
                 rules: [
                   {
                     required: true,
-                    message: "Please input your password!",
+                    message: 'Please input your password!',
                   },
                   {
                     validator: validateToNextPassword,
@@ -143,20 +141,20 @@ const RegistrationForm = (props) => {
                 ],
               })(
                 <Input.Password
-                  className="form-input"
-                  placeholder="Password"
+                  className='input'
+                  placeholder='Password'
                   prefix={
-                    <Icon type="lock" style={{ color: "rgba(0,0,0,.70)" }} />
+                    <Icon type='lock' style={{ color: 'rgba(0,0,0,.70)' }} />
                   }
                 />
               )}
             </Form.Item>
             <Form.Item hasFeedback>
-              {getFieldDecorator("confirm", {
+              {getFieldDecorator('confirm', {
                 rules: [
                   {
                     required: true,
-                    message: "Please confirm your password!",
+                    message: 'Please confirm your password!',
                   },
                   {
                     validator: compareToFirstPassword,
@@ -164,49 +162,44 @@ const RegistrationForm = (props) => {
                 ],
               })(
                 <Input.Password
-                  className="form-input"
+                  className='input'
                   onBlur={handleConfirmBlur}
-                  placeholder="Confirm Password"
+                  placeholder='Confirm Password'
                   prefix={
-                    <Icon type="lock" style={{ color: "rgba(0,0,0,.70)" }} />
+                    <Icon type='lock' style={{ color: 'rgba(0,0,0,.70)' }} />
                   }
                 />
               )}
             </Form.Item>
             <Form.Item {...tailFormItemLayout}>
-              <Button
-                type="primary"
-                htmlType="submit"
-                style={{ fontWeight: "bold", height: "5rem", fontSize:"1.6rem" }}
-              >
+              <button
+                type='primary'
+                htmltype='submit'
+                className='loginButtons loginButton createStore'>
                 Register
-              </Button>
+              </button>
             </Form.Item>
           </Form>
-<<<<<<< HEAD
           <div id='or_login' data-testid='loginLinkWrapper'>
-=======
-          <div id="or_login">
->>>>>>> 844aeeb2ed67d7c754ff8470c9ce4f08b42eb8ed
             <p>
-              or <Link to="/">login</Link> instead
+              or <Link to='/'>login</Link> instead
             </p>
           </div>
         </div>
       </div>
     </Spin>
-  )
+  );
 
-  return registerForm
-}
+  return registerForm;
+};
 
-const WrappedRegistrationForm = Form.create({ name: "register" })(
+const WrappedRegistrationForm = Form.create({ name: 'register' })(
   RegistrationForm
-)
+);
 
 const mapStateToProps = (state) => ({
   isLoading: state.user.isLoading,
   errors: state.user.errors,
-})
+});
 
-export default connect(mapStateToProps, null)(WrappedRegistrationForm)
+export default connect(mapStateToProps, null)(WrappedRegistrationForm);
