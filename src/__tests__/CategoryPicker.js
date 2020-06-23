dacbe03782e501fa56c64f4fbdd672292ea941dd
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-06-23 16:45:00 -0700

    first commit >> created test/CategoryPicker.js file, general imports needed

---
+++ src/__tests__/CategoryPicker.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import renderWithProviders from '../utlities/renderWithProviders'
+import CategoryPicker from '../components/categories/CategoryPicker'
+
