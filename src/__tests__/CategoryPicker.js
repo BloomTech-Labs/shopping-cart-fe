9a062712b8919011c7bea07785445dc5a1f9c238
Eric Ferguson <ferguson.eric.r@gmail.com>
2020-06-23 17:25:00 -0700

    test > All button + main div, optionWrapper

--- src/__tests__/CategoryPicker.js
+++ src/__tests__/CategoryPicker.js
@@ -1,5 +1,18 @@
-import React from 'react'
-import '@testing-library/jest-dom/extend-expect'
-import renderWithProviders from '../utlities/renderWithProviders'
-import CategoryPicker from '../components/categories/CategoryPicker'
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import renderWithProviders from "../utlities/renderWithProviders";
+import CategoryPicker from "../components/categories/CategoryPicker";
+import { exportAllDeclaration } from "@babel/types";
 
+describe("Category Picker in Store-NavBar", () => {
+  test("renders the All button", () => {
+    const { getByRole } = renderWithProviders(<CategoryPicker />);
+    const allButton = getByRole("button");
+    expect(allButton).toBeVisible();
+    expect(allButton).toBeEnabled();
+  });
+
+  test("renders optionWrapper, main div, for CategoryPicker", () => {
+    const 
+  }
+});
