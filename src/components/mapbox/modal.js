6b3992fee269691a2dbd38d990ba65ede2c7bc8a
Ariunbold <ariun1118@gmail.com>
2020-07-13 22:26:44 -0400

    fixed mapbox localstorage issue

--- src/components/mapbox/modal.js
+++ src/components/mapbox/modal.js
@@ -1,40 +1,25 @@
-import React, { useState, useEffect } from "react"
+import React, { useState } from "react"
 import ReactMapGL, {
   Marker,
   NavigationControl,
   GeolocateControl,
   Popup,
 } from "react-map-gl"
-import Geocode from "react-geocode"
 import location from "../../images/location.svg"
 
-const googleApiToken = "AIzaSyCGitfTdb5_Ws7YW5bw96WUk0MjlDknV7E"
 const mapboxApiToken =
   "pk.eyJ1IjoiYXJpdWthMTEiLCJhIjoiY2tjYjU4djRmMHE4azM0cWtsOWY0OXR4MSJ9.nXeE9BwATwbesLeJJxJTAw"
 
-const Modal = ({ show, user }) => {
+const Modal = ({ show, user, place }) => {
+  const local = JSON.parse(localStorage.getItem("response"))
   const [selected, setSelected] = useState(false)
 
-  useEffect(() => {
-    Geocode.setApiKey(googleApiToken)
-    Geocode.fromAddress(user.address + user.city)
-      .then((response) => {
-        window.localStorage.setItem("response", JSON.stringify(response))
-      })
-      .catch((err) => {
-        console.log(err.message)
-      })
-  }, [user])
-
-  const coordinate = JSON.parse(localStorage.getItem("response")).results[0]
-    .geometry.location
-
   const [viewPort, setViewPort] = useState({
-    latitude: coordinate.lat,
-    longitude: coordinate.lng,
+    latitude: local.lat,
+    longitude: local.lng,
     width: "30vw",
     height: "20vh",
-    zoom: 14,
+    zoom: 5,
   })
 
   return (
@@ -45,71 +30,77 @@ const Modal = ({ show, user }) => {
         opacity: show ? "1" : "0",
       }}
     >
-      <div className="modal-header">
-        <h3>
-          {user.businessName} | {user.address}, {user.city} {user.state},{" "}
-          {user.zipcode}
-        </h3>
-        <p>Hours: {user.hours}</p>
-        <p>Hours: 9:00am- 8:00pm || Monday-Friday</p>
-        <a
-          href={`https://www.google.com/maps/search/?api=1&query=${coordinate.lat},${coordinate.lng}`}
-        >
-          Get Direction
-        </a>
-      </div>
-      <div className="modal-body">
-        <div className="mapContainer">
-          <ReactMapGL
-            {...viewPort}
-            mapboxApiAccessToken={mapboxApiToken}
-            // mapStyle = "mapbox://styles/ariuka11/ckcgwlimk0ldc1iob71hc4s2h"
-            mapStyle="mapbox://styles/ariuka11/ckcgwq30x0a8k1ip68ajqruoo"
-            // mapStyle = "mapbox://styles/ariuka11/ckcb5g7ne3rbq1inu8kc7dqye"
-            onViewportChange={(viewPort) => {
-              setViewPort(viewPort)
-            }}
+      {place && (
+        <div className="modal-header">
+          <h3>
+            {user.businessName} | {user.address}, {user.city} {user.state},{" "}
+            {user.zipcode}
+          </h3>
+          <p>Hours: {user.hours}</p>
+          <p>Hours: 9:00am- 8:00pm || Monday-Friday</p>
+          <a
+            href={`https://www.google.com/maps/search/?api=1&query=${place.lat},${place.lng}`}
           >
-            <Marker
-              latitude={coordinate.lat}
-              longitude={coordinate.lng}
-              offsetLeft={-20}
-              className="marker"
-            >
-              <button
-                onClick={(e) => {
-                  e.preventDefault()
-                  setSelected(true)
-                }}
-              >
-                <img src={location} alt="marker" />
-              </button>
-            </Marker>
-            {selected && (
-              <Popup
-                latitude={coordinate.lat}
-                longitude={coordinate.lng}
-                onClose={() => {
-                  setSelected(false)
-                }}
-              >
-                <h3>{user.address},</h3>
-                <p style={{ fontSize: "1.5rem" }}>
-                  {user.city} {user.state}, {user.zipcode}
-                </p>
-              </Popup>
-            )}
-            <div style={{ position: "absolute", right: 0 }}>
-              <NavigationControl />
-            </div>
-            <div style={{ position: "absolute", left: 0 }}>
-              <GeolocateControl
-                positionOptions={{ enableHighAccuracy: true }}
-                trackUserLocation={true}
-              />
-            </div>
-          </ReactMapGL>
+            Get Direction
+          </a>
         </div>
+      )}
+      <div className="modal-body">
+        {place ? (
+          <div className="mapContainer">
+            <ReactMapGL
+              {...viewPort}
+              mapboxApiAccessToken={mapboxApiToken}
+              // mapStyle = "mapbox://styles/ariuka11/ckcgwlimk0ldc1iob71hc4s2h"
+              mapStyle="mapbox://styles/ariuka11/ckcgwq30x0a8k1ip68ajqruoo"
+              // mapStyle = "mapbox://styles/ariuka11/ckcb5g7ne3rbq1inu8kc7dqye"
+              onViewportChange={(viewPort) => {
+                setViewPort(viewPort)
+              }}
+            >
+              <Marker
+                latitude={place.lat}
+                longitude={place.lng}
+                offsetLeft={-20}
+                className="marker"
+              >
+                <button
+                  onClick={(e) => {
+                    e.preventDefault()
+                    setSelected(true)
+                  }}
+                >
+                  <img src={location} alt="marker" />
+                </button>
+              </Marker>
+              {selected && (
+                <Popup
+                  latitude={place.lat}
+                  longitude={place.lng}
+                  onClose={() => {
+                    setSelected(false)
+                  }}
+                >
+                  <h3>{user.address},</h3>
+                  <p style={{ fontSize: "1.5rem" }}>
+                    {user.city} {user.state}, {user.zipcode}
+                  </p>
+                </Popup>
+              )}
+              <div style={{ position: "absolute", right: 0 }}>
+                <NavigationControl />
+              </div>
+              <div style={{ position: "absolute", left: 0 }}>
+                <GeolocateControl
+                  positionOptions={{ enableHighAccuracy: true }}
+                  trackUserLocation={true}
+                />
+              </div>
+            </ReactMapGL>
+          </div>
+        ) : (
+          <div>No Map</div>
+        )}
       </div>
     </div>
   )
--- src/components/store/StoreNav.js
+++ src/components/store/StoreNav.js
@@ -1,15 +1,31 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { NavLink } from "react-router-dom"
 import { useSelector } from "react-redux"
 import history from "../../history"
 import search_icon from "../../images/search-icon.svg"
 import cart_icon from "../../images/cart-icon.svg"
 import Modal from "../mapbox/modal"
+import Geocode from "react-geocode"
+
+const googleApiToken = "AIzaSyC0K-idf45E3-_5IAjc0PXB-5Ug6S4e9ag"
 
 const StoreNav = (props) => {
   const [show, setShow] = useState(false)
   const cartContents = useSelector((state) => state.cart)
   const store = useSelector((state) => state.user.user)
+  const [place, setPlace] = useState()
+
+  useEffect(() => {
+    Geocode.setApiKey(googleApiToken)
+    Geocode.fromAddress(store.address + store.city)
+      .then((response) => {
+        setPlace(response.results[0].geometry.location)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  }, [store.address, store.city])
+
   const findRef = window.location.href
   const totalQuantity = (arr) => {
     return arr.reduce((sum, item) => {
@@ -60,7 +76,7 @@ const StoreNav = (props) => {
         <button className="aboutUs" onClick={openModal}>
           About Us
         </button>
-        <Modal className="modal" show={show} close={closeModal} user={store} />
+        <Modal className="modal" show={show} close={closeModal} user={store} place = {place}/>
         <div
           data-testid="badge"
           className="badge"
